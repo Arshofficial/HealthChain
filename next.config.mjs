7c1700eec8e6d8e541f71c4884f62cd6a89d50ec
Arsh <utkarshupadhyayofficial@gmail.com>
2025-04-29 02:35:50 +0530

    Fix: Serverless deployment for dynamic routes on Netlify

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
-export const dynamic = "force-dynamic";
 
 const nextConfig = {
-  output: "export",   // <-- THIS MUST BE PRESENT 🔥🔥🔥
   images: {
     unoptimized: true,
   },
